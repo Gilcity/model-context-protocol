42a4040e402c545740f0deed8e57a332e0f5ab72
Gilcity <gil.cty@gmail.com>
2025-10-30 15:55:01 -0700

    grabbing the highest ticker

--- tests/playwright_test.py
+++ tests/playwright_test.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import sync_playwright, TimeoutError as PWTimeout
 import time 
+
 #handling cookie popups IF IT APPEARS
 def accept_cookies(page): 
     try: 
@@ -14,9 +15,14 @@ def accept_cookies(page):
         print(f"[warn] Could not click cookies banner {e}")
     return False
 
-#searching for the highest stock that day 
-#def search(page): 
-
+#finding the highest ticker and its corresponding price
+def search(page): 
+    page.wait_for_selector("table tbody tr", timeout=30000)
+    first_row = page.query_selector("table tbody tr")
+    if not first_row: 
+        return None
+    symbol_cell = first_row.query_selector("td a")
+    return symbol_cell.inner_text().strip() if symbol_cell else None 
 #report price 
 #def report(page): 
 
@@ -30,18 +36,14 @@ def test_open_yahoo():
             #load and handle cookies
             page.set_default_timeout(60000)
             page.goto("https://finance.yahoo.com/markets/stocks/gainers/?fr=sycsrp_catchall", wait_until="domcontentloaded") 
-            #  page.get_by_text("Search for news, tickers or companies").click()
             accept_cookies(page)
-        
+
+            #finding top gainer 
+            page.wait_for_selector("table tbody tr", timeout=30000)
+            top = search(page)
+            print(f"Top gainer symbol: {top}")                   
        # assert "" in page.title()
         #browser.close()
-            try:
-                page.wait_for_selector("table", timeout=4000) #40 sec timeout
-                print("✅ Page loaded and table found.")
-            except PWTimeout:
-                print("Table not found, but page may still be usable.")
-                print(f"Page title: {page.title()}")
-
         except PWTimeout:
             print(" Timeout while loading Yahoo Finance.")
         finally:
